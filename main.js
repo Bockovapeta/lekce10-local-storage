ab3cfb59f0864e25fd61d59697b6f9e1b1037f11
Petra Bočková <Wickky@seznam.cz>
2022-04-19 19:33:59 +0200

    function hodnota

--- main.js
+++ main.js
@@ -1,28 +1,33 @@
 // do proměnné si uložíme prvek ve stránce,
 // kam budeme vypisovat výsledek
+const vysledek = document.querySelector('#vysledek');
 
 // ihned po načtení stránky zavoláme funkci,
 // která přečte hodnotu z Local Storage a zobrazí ji
-
+nactiHodnotu();
 // funkce pro načtení a zobrazení hodnoty z Local Storage
 function nactiHodnotu() {
 	// načteme z Local Storage
-
+	let hodnota = localStorage.getItem('mojehodnota');
 	// pokud hodnota nexistuje, vypíšeme chybu
-		
+	if (hodnota === null || hodnota === undefined) {
+		vysledek.textContent = 'V local storage neexistuje "moje hodnota".';
+	}
 	// jinak hodnota existuje a vypíšeme ji
-
+	else {
+		vysledek.textContent = hodnota;
+	}
 }
 
 // funkce pro uložení hodnoty do Local Storage
 function ulozHodnotu() {
 	// hodnotu načteme z pole formuláře
-
+	let hodnota = document.querySelector('#hodnota').value;
 	// a uložíme do local storage pod jménem "mojehodnota"
-
+	localStorage.setItem('mojehodnota', hodnota);
 	// a hned zavoláme funkci pro zobrazení hodnoty,
 	// aby se změna v Local Storage promítla i na stránce
-
+	nactiHodnotu();
 }
 
 // funkce pro smazání hodnoty z Local Storage
@@ -32,5 +37,8 @@ function smazatHodnotu() {
 
 	// a hned zavoláme funkci pro zobrazení hodnoty,
 	// aby se změna v Local Storage promítla i na stránce
+	localStorage.removeItem('mojehodnota');
+	nactiHodnotu();
+
 
 }
